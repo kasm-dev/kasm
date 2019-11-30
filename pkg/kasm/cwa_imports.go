e0066feeb8663aa1d612e105d1842acd35de9769
Adrien <adrien@arccoza.com>
2019-12-01 04:22:10 +0900

    Move Common WebAssembly imports into their own file

---
+++ pkg/kasm/cwa_imports.go
@@ -0,0 +1,31 @@
+package kasm
+
+// #include <stdlib.h>
+//
+// extern int32_t io_get_stdout(void *context);
+// extern int32_t resource_write(void *context, int32_t id, int32_t ptr, int32_t len);
+import "C"
+import (
+  "unsafe"
+
+  wasm "github.com/wasmerio/go-ext-wasm/wasmer"
+)
+
+//export io_get_stdout
+func io_get_stdout(context unsafe.Pointer) int32 {
+	return 1
+}
+
+//export resource_write
+func resource_write(context unsafe.Pointer, id, ptr, len int32) int32 {
+	return 0
+}
+
+func appendCWAImports(imports *wasm.Imports) (*wasm.Imports, error) {
+	if _, err := imports.Append("io_get_stdout", io_get_stdout, C.io_get_stdout); err != nil {
+		return nil, err
+	} else if _, err = imports.Append("resource_write", resource_write, C.resource_write); err != nil {
+		return nil, err
+	}
+	return imports, nil
+}
--- pkg/kasm/runtime.go
+++ pkg/kasm/runtime.go
@@ -1,13 +1,8 @@
 package kasm
 
-// #include <stdlib.h>
-//
-// extern int32_t io_get_stdout(void *context);
-// extern int32_t resource_write(void *context, int32_t id, int32_t ptr, int32_t len);
-import "C"
 import (
 	"fmt"
-	"unsafe"
+	// "unsafe"
 
 	// cri "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 	// "github.com/perlin-network/life"
@@ -25,8 +20,7 @@ func (c *Container) AddImport(bytes []byte) error {
 
 func (c *Container) Load(bytes []byte) error {
 	var err error
-	imports, _ := wasm.NewImports().Append("io_get_stdout", io_get_stdout, C.io_get_stdout)
-	imports, _ = imports.Append("resource_write", resource_write, C.resource_write)
+	imports, _ := appendCWAImports(wasm.NewImports())
 	c.inst, err = wasm.NewInstanceWithImports(bytes, imports)
 	// c.inst, err = wasm.NewInstance(bytes)
 	fmt.Println(err, c.inst.Exports)
@@ -44,14 +38,4 @@ func (c *Container) LoadPath(path string) error {
 	} else {
 		return nil
 	}
-}
-
-//export io_get_stdout
-func io_get_stdout(context unsafe.Pointer) int32 {
-	return 1
-}
-
-//export resource_write
-func resource_write(context unsafe.Pointer, id, ptr, len int32) int32 {
-	return 0
 }
