1f835e2848e36063f6d0c22aba41eaacd5966928
Adrien <adrien@arccoza.com>
2019-12-05 13:10:44 +0900

    Update Common WebAssembly ABI

--- pkg/kasm/abi/cwa.go
+++ pkg/kasm/abi/cwa.go
@@ -2,22 +2,30 @@ package abi
 
 // #include <stdlib.h>
 //
-// extern int32_t io_get_stdout(void *context);
-// extern int32_t resource_write(void *context, int32_t id, int32_t ptr, int32_t len);
+// extern int32_t io_get_stdout(void *ctxp);
+// extern int32_t resource_write(void *ctxp, int32_t id, int32_t ptr, int32_t len);
 import "C"
 import (
+	"fmt"
   "unsafe"
-  
+
   wasm "github.com/wasmerio/go-ext-wasm/wasmer"
 )
 
 //export io_get_stdout
-func io_get_stdout(context unsafe.Pointer) int32 {
+func io_get_stdout(ctxp unsafe.Pointer) int32 {
+	fmt.Println("io_get_stdout")
 	return 1
 }
 
 //export resource_write
-func resource_write(context unsafe.Pointer, id, ptr, len int32) int32 {
+func resource_write(ctxp unsafe.Pointer, fd, ptr, len int32) int32 {
+	ctx := wasm.IntoInstanceContext(ctxp)
+	fmt.Println("resource_write", fd, ptr, len)
+	if fd == 1 {
+		out := ctx.Memory().Data()[ptr:]
+		fmt.Print(string(out[:len]), out[:len])
+	}
 	return 0
 }
 
